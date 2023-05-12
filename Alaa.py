ac7648c63695d89180ccbe25ea94fc6c0c1dc28e
elmasri0 <126913844+elmasri0@users.noreply.github.com>
2023-05-12 15:54:50 +0300

    Update Alaa.py

--- Alaa.py
+++ Alaa.py
@@ -16,7 +16,7 @@ A = '\033[2;34m'#ازرق
 C = '\033[2;35m' #وردي
 B = '\033[2;36m'#سمائي
 Y = '\033[1;34m' #ازرق فاتح
-os=pyfiglet.figlet_format       ( "     __ F L E X __")
+os=pyfiglet.figlet_format       ( "     __ F L E X..g __")
 print(Z+os)
 
 print('_'*65)
