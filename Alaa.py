9000ca786df69491ef98a46a462dd8db01477fd1
elmasri0 <126913844+elmasri0@users.noreply.github.com>
2023-05-19 20:43:25 +0300

    Update Alaa.py

--- Alaa.py
+++ Alaa.py
@@ -16,7 +16,7 @@ A = '\033[2;34m'#ازرق
 C = '\033[2;35m' #وردي
 B = '\033[2;36m'#سمائي
 Y = '\033[1;34m' #ازرق فاتح
-os=pyfiglet.figlet_format       ( "     __ F L E X..g __")
+os=pyfiglet.figlet_format       ( "     __ F L E X __")
 print(Z+os)
 
 print('_'*65)
@@ -241,7 +241,7 @@ jsona={"category": [{"listHierarchyId": "PackageID", "value": "523"}, {"listHier
     
 lokaa=requests.patch(urla2, headers=hda, json=jsona).text
 #print(loka)
-if str('{}') in str(rreqa4):
+if str('{}') in str(lokaa):
 
 	print("{}")
 else:
