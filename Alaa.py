74c40b585ab8152d555930b73e10a02ecc5a5c67
elmasri0 <126913844+elmasri0@users.noreply.github.com>
2023-05-20 14:25:25 +0300

    Update Alaa.py

--- Alaa.py
+++ Alaa.py
@@ -16,16 +16,16 @@ A = '\033[2;34m'#ازرق
 C = '\033[2;35m' #وردي
 B = '\033[2;36m'#سمائي
 Y = '\033[1;34m' #ازرق فاتح
-os=pyfiglet.figlet_format       ( "     __ F L E X..g __")
+os=pyfiglet.figlet_format       ( "     __ F L E X.__")
 print(Z+os)
 
 print('_'*65)
 os=("by: MR ALAA")
 print(lll+os)
 print('_'*65)
-number=("01002836435")
+number=("01008965284")
 print()
-password=("Hesham@@2")
+password=("Alla@@123")
 print()
 url="https://mobile.vodafone.com.eg/auth/realms/vf-realm/protocol/openid-connect/token"
 
